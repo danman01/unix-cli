78a8ceca36faa4b334b2966dcd1154f406881d90
Danny Kirschner <rkirschner1377@gmail.com>
2019-12-02 08:48:36 -0500

    Turning into interactive unix cheatsheet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Landlord'`, () => {
+  it(`should have as title 'Unix-CLI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Landlord');
+    expect(app.title).toEqual('Unix-CLI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Landlord app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Unix-CLI app is running!');
   });
 });
