6a085b63beb35170c0771cc2e56ba6c9cf00b0ea
Danny Kirschner <rkirschner1377@gmail.com>
2019-12-02 10:53:21 -0500

    Updating project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-tour-of-heroes app is running!');
+    expect(page.getTitleText()).toEqual('Unix CLI');
   });
 
   afterEach(async () => {
